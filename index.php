89311b26b6eea361f4a8e5b1aeb3f651b36ca845
Esucia <tautvydasstonys@gmail.com>
2023-02-20 12:48:31 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+use models\Category;
+use eftec\bladeone\BladeOne;
+require_once "vendor/autoload.php";
+
+
+if (isset($_GET['delete'])){
+    Category::get($_GET['delete'])->delete();
+    header("location: index.php");
+    die();
+}
+
+
+$blade=new BladeOne( __DIR__."/views",__DIR__."/compile",BladeOne::MODE_DEBUG );
+echo $blade->run("index");
+
---
+++ products.php
@@ -0,0 +1,14 @@
+<?php
+
+use eftec\bladeone\BladeOne;
+use models\Category;
+
+require_once "vendor/autoload.php";
+
+$id=$_GET['id'];
+$category=Category::get($id);
+
+$blade=new BladeOne( __DIR__."/views",__DIR__."/compile",BladeOne::MODE_DEBUG );
+echo $blade->run("products",[
+    "category"=>$category
+]);
